28603652a3c5a49be5ef90849ba195dcc39b52ac
Becca Elenzil <becca@adadevelopersacademy.org>
2022-04-25 12:13:40 -0700

    update cors config

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 def create_app(test_config=None):
     app = Flask(__name__)
     CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
     from .routes import proxy_bp
     app.register_blueprint(proxy_bp)
