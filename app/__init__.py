4f1174f37380d96669b9a386a462e326de9963bd
Becca Elenzil <becca@adadevelopersacademy.org>
2022-04-25 11:15:16 -0700

    complete get_lat_lon and get_weather routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    CORS(app)
 
     from .routes import proxy_bp
     app.register_blueprint(proxy_bp)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
@@ -12,9 +13,29 @@ weather_key = os.environ.get("WEATHER_KEY")
 
 @proxy_bp.route("/location", methods=["GET"])
 def get_lat_lon():
-    return "Lat, Lon!"
+    loc_query = request.args.get("q")
+    if not loc_query:
+        return {"message": "must provide q parameter (location)"}
+
+    response = requests.get(
+        "https://us1.locationiq.com/v1/search.php",
+        params={"q": loc_query, "key": location_key, "format": "json"}
+    )
+
+    print(response.json())
+    return jsonify(response.json())
 
 
 @proxy_bp.route("/weather", methods=["GET"])
 def get_weather():
-    return "Weather!"
+    lat_query = request.args.get("lat")
+    lon_query = request.args.get("lon")
+    print(lat_query, lon_query)
+    if not lat_query or not lon_query:
+        return {"message": "must provide lat and lon parameters"}
+
+    response = requests.get(
+        "https://api.openweathermap.org/data/2.5/onecall",
+        params={"lat": lat_query, "lon": lon_query, "appid": weather_key}
+    )
+    return response.json()
