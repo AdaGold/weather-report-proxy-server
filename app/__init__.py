5ee5c6e701a1577c6fe19ae9f090b9be0a1cd80d
Becca Elenzil <becca@adadevelopersacademy.org>
2022-04-25 08:26:03 -0700

    add weather and location routes

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,7 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    from .routes import proxy_bp
+    app.register_blueprint(proxy_bp)
+
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,18 @@
-@app.route("/hello-world", methods=["GET"])
-def say_hello_world():
-    return "Hello, World!"
+
+from flask import Blueprint
+
+load_dotenv()
+
+proxy_bp = Blueprint("proxy_bp", __name__)
+location_key = os.environ.get("LOCATION_KEY")
+weather_key = os.environ.get("WEATHER_KEY")
+
+
+@proxy_bp.route("/location", methods=["GET"])
+def get_lat_lon():
+    return "Lat, Lon!"
+
+
+@proxy_bp.route("/weather", methods=["GET"])
+def get_weather():
+    return "Weather!"
